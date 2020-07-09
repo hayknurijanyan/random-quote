4cb1d8c3df1476819f9fe063daed783986481e3b
Hayk <58630378+hayknurijanyan@users.noreply.github.com>
2020-07-09 12:49:57 +0400

    Update main.js

--- main.js
+++ main.js
@@ -7,12 +7,16 @@ author.innerHTML = '';
 quote.innerHTML = 'Press to start :)';
 button.addEventListener('click',randomQuote)
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 
 async function randomQuote() {
-    const response = await fetch ('https://thesimpsonsquoteapi.glitch.me/quotes')
+    const response = await fetch ('https://type.fit/api/quotes')
     let data = await response.json();
-    let m =  [...data][0];
-    author.innerHTML = m.character;
-    quote.innerHTML = `"${m.quote}"`;
+     let m =  [...data][getRandomInt(1642)];
+     console.log(m)
+     author.innerHTML = m.author;
+    quote.innerHTML = `"${m.text}"`;
     
 }
